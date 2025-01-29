8711178c6589054d9a1eab24b6b59f5b0207b297
Chandrayan Paul <chandrayanpaul27@gmail.com>
2025-01-29 20:15:30 +0530

    last commit

--- index.js
+++ index.js
@@ -46,6 +46,23 @@ app.post('/',(req,res)=>{
     res.status(200).render('index');
 })
 
+
+const url = `https://forms-haa5.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
